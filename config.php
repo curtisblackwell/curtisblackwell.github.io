52f649a88b7be9f7c662dee2fd482077837b7461
Curtis Blackwell <blackwell.curtis@gmail.com>
2021-10-26 21:02:54 -0500

    Update config

--- config.php
+++ config.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Str;
 return [
     'baseUrl' => '',
     'production' => false,
-    'siteName' => 'Blog Starter Template',
-    'siteDescription' => 'Generate an elegant blog with Jigsaw',
-    'siteAuthor' => 'Author Name',
+    'siteName' => 'Persfessional Growth',
+    'siteDescription' => 'Writings about my personal and professional growth.',
+    'siteAuthor' => 'Curtis Blackwell',
 
     // collections
     'collections' => [
         'posts' => [
-            'author' => 'Author Name', // Default author, if not provided in a post
+            'author' => 'Curtis Blackwell', // Default author, if not provided in a post
             'sort' => '-date',
             'path' => 'blog/{filename}',
         ],
