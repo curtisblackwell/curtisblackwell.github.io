92f7349df029cd894158f4b420d32cd35658e1bf
Curtis Blackwell <blackwell.curtis@gmail.com>
2021-10-26 20:45:14 -0500

    Update BrowserSync config to open in Brave Browser Nightly

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,6 +8,7 @@ mix.js('source/_assets/js/main.js', 'js').vue()
   .sass('source/_assets/sass/main.scss', 'css/main.css')
   .jigsaw({
     watch: ['config.php', 'source/**/*.md', 'source/**/*.php', 'source/**/*.scss'],
+    browserSyncOptions: { browser: 'Brave Browser Nightly' },
   })
   .options({
     processCssUrls: false,
