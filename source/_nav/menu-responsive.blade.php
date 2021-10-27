f3fdd18580665cc1c3fb2f0fe339dba48faead18
Curtis Blackwell <blackwell.curtis@gmail.com>
2021-10-26 21:36:01 -0500

    Remove Contact page

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -1,4 +1,4 @@
-<nav id="js-nav-menu" class="nav-menu hidden lg:hidden">
+<nav id="js-nav-menu" class="hidden nav-menu lg:hidden">
     <ul class="my-0">
         <li class="pl-4">
             <a
@@ -13,13 +13,6 @@
                 href="/about"
                 class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/about') ? 'active text-blue' : '' }}"
             >About</a>
-        </li>
-        <li class="pl-4">
-            <a
-                title="{{ $page->siteName }} Contact"
-                href="/contact"
-                class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/contact') ? 'active text-blue' : '' }}"
-            >Contact</a>
         </li>
     </ul>
 </nav>
