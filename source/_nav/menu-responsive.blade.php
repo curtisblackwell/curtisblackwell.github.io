0cf90212c0e411a45b78a1889dcb104b982f99b5
Curtis Blackwell <blackwell.curtis@gmail.com>
2021-11-09 08:41:32 -0600

    Replace about link with home

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -2,17 +2,17 @@
     <ul class="my-0">
         <li class="pl-4">
             <a
+                title="Home"
+                href="/"
+                class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/') ? 'active text-blue' : '' }}"
+            >Home</a>
+        </li>
+        <li class="pl-4">
+            <a
                 title="{{ $page->siteName }} Blog"
                 href="/blog"
                 class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/blog') ? 'active text-blue' : '' }}"
             >Blog</a>
-        </li>
-        <li class="pl-4">
-            <a
-                title="{{ $page->siteName }} About"
-                href="/about"
-                class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/about') ? 'active text-blue' : '' }}"
-            >About</a>
         </li>
     </ul>
 </nav>
