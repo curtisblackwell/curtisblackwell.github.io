c7e52c50da418fc9211f55346eddb086f2f27a07
Curtis Blackwell <blackwell.curtis@gmail.com>
2021-12-31 15:16:09 -0600

    Make more readable

--- source/inspiring-people.blade.php
+++ source/inspiring-people.blade.php
@@ -8,27 +8,30 @@ description: These people inspire me to think about things differently and appro
 
 
 @section('body')
-    @md
-        These people inspire me to think about things differently and approach problems smartlier.
+    <section class="lg:w-2/3">
+        @md
+            # Inspiring People
+            These people inspire me to think about things differently and approach problems smartlier.
 
-        ## Software development
-        - [Eva PenzeyMoog](https://twitter.com/epenzeymoog) shares a lot about [Designing for Safety](https://abookapart.com/products/design-for-safety).
-        - [Chelsea Troy](https://twitter.com/HeyChelseaTroy) teaches software development and surrounding skills and seems super busy making everything better.
-        - [GeePaw Hill](https://twitter.com/GeePawHill) muses about extracting value through software development faster by taking Many More Much Smaller Steps.
-
-
-        ## General management and career wisdom
-        - [Sara Wachter-Boettcher](https://twitter.com/sara_ann_marie) writes about organizational resilience through taking care of people.
-        - [Sherrell Dorsey](https://twitter.com/Sherrell_Dorsey) reframes common thinking in ingenius ways to reveal flaws and better approaches.
+            ## Software development
+            - [Eva PenzeyMoog](https://twitter.com/epenzeymoog) radiates empathy and wrote the book on [Designing for Safety](https://abookapart.com/products/design-for-safety).
+            - [Chelsea Troy](https://twitter.com/HeyChelseaTroy) teaches software development and surrounding skills. She seems super busy making pretty much everything better.
+            - [GeePaw Hill](https://twitter.com/GeePawHill) muses about extracting value through software development faster by taking [Many More Much Smaller Steps](https://www.geepawhill.org/series/many-much-more-smaller-steps/).
 
 
-        ## Service design? Thinking about systems
-        - [Majid Iqbal](https://twitter.com/mxiqbal) wrote a great book called [*Thinking in Services*](https://www.bispublishers.com/thinking-in-services.html). I've barely gotten into it, but it's already changed the ways I think about pretty much everything. I just haven't had the time and energy to finish it yet.
-        - [Kent Beck](https://twitter.com/KentBeck) is talking a lot about [incentives](https://geekincentives.substack.com) and how we'd be better off thinking through those we intentionally and unintentionally create.
+            ## General management and career wisdom
+            - [Sara Wachter-Boettcher](https://twitter.com/sara_ann_marie) writes about organizational resilience through caring for people.
+            - [Sherrell Dorsey](https://twitter.com/Sherrell_Dorsey) reframes common thinking in ingenius ways to reveal flaws and better approaches.
 
 
-        ## Identifying and interrupting shitty thought patterns and behavior
-        - [Em](https://twitter.com/emrazz) is a master of explaining some pretty fucked up patterns of thought and behavior a lot of us (vast majority of us men) engage in.
-        - [Carys](https://twitter.com/blindandb0ujee) talks a lot about accessibility and disability, digital and not.
-    @endmd
+            ## Service design? Thinking about systems
+            - [Majid Iqbal](https://twitter.com/mxiqbal) wrote a great book called [*Thinking in Services*](https://www.bispublishers.com/thinking-in-services.html). I've barely gotten into it, but it's already changed the ways I think about pretty much every interaction. I just haven't had the time and energy to finish it yet.
+            - [Kent Beck](https://twitter.com/KentBeck) is talking a lot about [incentives](https://geekincentives.substack.com) and how we'd be better off thinking through those we intentionally and unintentionally create.
+
+
+            ## Identifying and interrupting shitty thought patterns and behavior
+            - [Em](https://twitter.com/emrazz) is a master of explaining some pretty fucked up patterns of thought and behavior a lot of us (the vast majority of us men) engage in.
+            - [Carys](https://twitter.com/blindandb0ujee) talks a lot about accessibility and disability, digital and not.
+        @endmd
+    </section>
 @endsection
